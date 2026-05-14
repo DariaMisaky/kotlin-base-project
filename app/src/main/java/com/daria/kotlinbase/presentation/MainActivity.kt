146802c54caa820aac3365d61568f5d70bac2e73
Daria <daria@wolfpack-digital.com>
2026-05-14 23:15:06 +0300

    feat: enable edge-to-edge in MainActivity, apply fitsSystemWindows on activity root

--- app/src/main/java/com/daria/kotlinbase/presentation/MainActivity.kt
+++ app/src/main/java/com/daria/kotlinbase/presentation/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.daria.kotlinbase.presentation
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.daria.kotlinbase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
